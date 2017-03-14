624ce195964e1d8e7833937b3d208ca9f4503c9d
Rob Redford <Rom3oDelta7@outlook.com>
2017-03-13 17:10:41 -0700

    ASk for a conformation command before clearing the credentials

--- Hosts/Esp8266/Extras/ClearCredentials/ClearCredentials.ino
+++ Hosts/Esp8266/Extras/ClearCredentials/ClearCredentials.ino
@@ -7,28 +7,42 @@
 #include <EEPROM.h>
 #include <WiFiManager.h>
 
-#define  EEPROM_START       0
+#define  EEPROM_START       0           // per WiFiManager documentation (readme.md)
 #define  EEPROM_SIZE        128
 
 
 void setup(void) {
    Serial.begin(74880);
    EEPROM.begin(EEPROM_SIZE);
+
+   Serial.print(F("\nEnter 'c' to clear saved WiFi credentials: "));
 }
 
 void loop(void) {
-   WiFi.disconnect(true);               // clears SDK-stored SSID and password
-   Serial.println(F("(1)SDK credentials cleared"));
+   char buffer[65];                     // hw serial buffer is 64 bytes
 
-   // erase saved EEPROM credentials
-   if ( EEPROM.read(EEPROM_START) == EEPROM_KEY ) {
-      for ( int i = 0; i < EEPROM_SIZE; i++ ) {
-         EEPROM.write(i, 0);
+   //Serial.flush();                     // no longer flushes remaining input :-(
+   if ( Serial.readBytesUntil('\n', buffer, 64) ) {
+      if ( buffer[0] == 'c' ) {
+         Serial.println(F("\n\nClearing all saved WiFi credentials...\n"));
+
+         WiFi.disconnect(true);               // clears SDK-stored SSID and password
+         Serial.println(F("(1)SDK credentials cleared"));
+
+         // erase saved EEPROM credentials
+         if ( EEPROM.read(EEPROM_START) == EEPROM_KEY ) {
+            for ( int i = 0; i < EEPROM_SIZE; i++ ) {
+               EEPROM.write(i, 0);
+            }
+            EEPROM.commit();
+            Serial.println(F("(2)EEPROM credentials cleared"));
+         } else {
+            Serial.println(F("(2)No saved EEPROM credentials"));
+         }
+         // wait here
+         while ( true ) delay(1000);                      // need delay to avoid WDT reset
+      } else {
+         Serial.print(F("\nEnter 'c' to clear saved WiFi credentials: "));
       }
-      EEPROM.commit();
-      Serial.println(F("(2)EEPROM credentials cleared"));
-   } else {
-      Serial.println(F("(2)No saved EEPROM credentials"));
    }
-   while ( true ) delay(5000);                  // delay is requried to avoid WDT reset
 }
