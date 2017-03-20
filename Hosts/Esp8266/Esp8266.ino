33b5ac892b4ad29fbc5442a2fb99381f60e06c67
Rob Redford <Rom3oDelta7@outlook.com>
2017-03-20 10:39:11 -0700

    Pick up Maurice's fix in esp8266.ino

--- Hosts/Esp8266/Esp8266.ino
+++ Hosts/Esp8266/Esp8266.ino
@@ -452,7 +452,7 @@ uint16 genPacketSize(uint8 b0, uint8 b1) {
 
 uint8 getPacketSize(uint16 val, uint8 byteNumber) {
   if (byteNumber == 0) {
-    return val && 0xFF;
+    return uint8(val & 0xFF);
   } else {
     return val >> 8;
   }
