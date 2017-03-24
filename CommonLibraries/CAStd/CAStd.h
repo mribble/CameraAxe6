dc4e7db02b6dda2cdedeab00de070ecdbdc19dd8
Rob Redford <Rom3oDelta7@outlook.com>
2017-03-24 14:11:07 -0700

    Rewrite of CAStd.h to use common logging function and provide infividual control over ERROR, INFO, ASSERT, and LOG messages Provide a short example sketch showing use of the message macros

--- CommonLibraries/CAStd/CAStd.h
+++ CommonLibraries/CAStd/CAStd.h
@@ -4,34 +4,57 @@
 /*
  Common macros for SAM3x and ESP8266 code
  
- CA_DEBUG and ESP_ALT_CONSOLE symbols must be defined (enable debug output) or not defined (no debug output)
+ The symbols: CA_DEBUG_ERROR, CA_DEBUG_INFO, CA_DEBUG_LOG, CA_DEBUG_ASSERT 
+ must be defined (enable output) or not defined (no output)
  before including this header file
+ 
+ Likewise, for the ESP8266, SerialIO must also be defined first (Serial or Serial1)
  */
 
-#ifdef CA_DEBUG
-
-#ifdef ESP8266
-#ifdef ESP_ALT_CONSOLE
-#define SerialIO      Serial1        // UART on GPIO2 - use a pullup (this is necessary for boot anyway)
-#else
-#define SerialIO      Serial
-#endif // ESP_ALT_CONSOLE
-#elif __SAM3X8E__
-#define SerialIO      SerialUSB      // native USB port
+#if defined(__SAM3X8E__) && not defined(SerialIO)
+#define SerialIO  SerialUSB
 #endif
 
-#define CA_MSG(header, msg, value)     \
-   {                                   \
-      SerialIO.print(header);          \
-      SerialIO.print(msg);             \
-      SerialIO.print(F(": "));         \
-      SerialIO.println(value);         \
-   } 
-#define CA_INFO(msg, value)        CA_MSG(F("INFO> "), msg, value)
-#define CA_ERROR(msg, value)       CA_MSG(F("ERROR> "), msg, value)
 
-#define CA_ASSERT(cond, str)               \
-   do {                                    \
+#ifdef CA_DEBUG_LOG
+/* 
+  Base function to print debug messages
+  Requires the format string to use the PSTR() macro
+   
+  We need this for SAM3X becuase there is no Serial.printf function available
+  While there is a Serial.print available for the ESP8266, it is best to maintain consistency with the use of PSTR()
+  
+  This function should not be called directly - only through the macro below
+ */
+void CALog(PGM_P fmt, ...) __attribute__((format(printf, 1, 2)));
+
+void CALog(PGM_P fmt, ...) {
+   const uint8_t maxSize = 128;                        // Max resulting string size
+   char          buf[maxSize]; 
+   va_list       args;
+	 
+	va_start (args, fmt);
+#ifdef __SAM3X8E__
+	char format[maxSize];                               // SAM3X has no vsnprintf_P so copy into a char array first
+	strncpy_P(format, fmt, maxSize);
+   vsnprintf(buf, maxSize, format, args);
+#elif ESP8266
+   vsnprintf_P(buf, maxSize, fmt, args);
+#endif
+   va_end(args);
+   SerialIO.print(buf);
+}
+
+// for the CA_LOG function, you *must* use the PSTR() macro for the format string
+#define CA_LOG(fmt, ...)           CALog(fmt, ##__VA_ARGS__)
+
+#else 
+	#define CA_LOG(...)
+#endif // LOG
+
+#ifdef CA_DEBUG_ASSERT
+   #define CA_ASSERT(cond, str)            \
+   {                                       \
       if(!(cond)) {                        \
          SerialIO.print(__FILE__);         \
          SerialIO.print(F(" line("));      \
@@ -41,15 +64,32 @@
          SerialIO.print(F(") -- "));       \
          SerialIO.println(str);            \
       }                                    \
-   } while ( false )
+   }
+#else
+	#define CA_ASSERT(...)
+#endif
+	
+#define _CA_MSG(header, msg, value)    \
+   {                                   \
+      SerialIO.print(header);          \
+      SerialIO.print(msg);             \
+      SerialIO.print(F(": "));         \
+      SerialIO.println(value);         \
+   } 
+	
+// functions for simple messages using Serial.print. Use the F() macro for strings
+#ifdef CA_DEBUG_INFO
+   #define CA_INFO(msg, value)        _CA_MSG(F("INFO> "), msg, value)
+#else
+	#define CA_INFO(...)
+#endif
 
-#else // CA_DEBUG
+#ifdef CA_DEBUG_ERROR
+   #define CA_ERROR(msg, value)       _CA_MSG(F("ERROR> "), msg, value)
+#else
+	#define CA_ERROR(...)
+#endif
 
-#define SerialIO         Serial      // for ESP8266, this will send (non-debug) connection messages to Serial
-#define CA_INFO(...) 
-#define CA_ERROR(...) 
-#define CA_ASSERT(...)
 
-#endif // CA_DEBUG
 
-#endif //__CASTD_H__
+#endif 
