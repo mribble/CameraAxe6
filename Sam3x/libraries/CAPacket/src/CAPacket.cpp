bfe5534b8386da639ac0eb6c01097e55be4d13cf
Maurice Ribble <maurice@dreamingrobots.com>
2017-06-21 16:55:30 -0400

    Clean up base packet classes

--- Sam3x/libraries/CAPacket/src/CAPacket.cpp
+++ Sam3x/libraries/CAPacket/src/CAPacket.cpp
@@ -679,6 +679,65 @@ uint16_t CAPacketLogger::pack() {
 }
 
 ///////////////////////////////////////////////////////////////////////////////
+// CamSettings Packet Class
+///////////////////////////////////////////////////////////////////////////////
+CAPacketCamSettingsBase::CAPacketCamSettingsBase() {
+    mCamPortNumber = 0;
+    mMode = 0;
+    mDelayHours = 0;
+    mDelayMinutes = 0;
+    mDelaySeconds = 0;
+    mDelayMilliseconds = 0;
+    mDelayMicroseconds = 0;
+    mDurationHours = 0;
+    mDurationMinutes = 0;
+    mDurationSeconds = 0;
+    mDurationMilliseconds = 0;
+    mDurationMicroseconds = 0;
+    mSequencer = 0;
+    mApplyIntervalometer = 0;
+    mSmartPreview = 0;
+    mMirrorLockupEnable = 0;
+    mMirrorLockupMinutes = 0;
+    mMirrorLockupSeconds = 0;
+    mMirrorLockupMilliseconds = 0;
+}
+
+void CAPacketCamSettingsBase::set(uint8_t camPortNumber, uint8_t mode, uint16_t delayHours, uint8_t delayMinutes,
+                uint8_t delaySeconds, uint16_t delayMilliseconds, uint16_t delayMicroseconds,
+                uint16_t durationHours, uint8_t durationMinutes, uint8_t durationSeconds, 
+                uint16_t durationMilliseconds, uint16_t durationMicroseconds, uint8_t sequencer,
+                uint8_t applyIntervalometer, uint8_t smartPreview, uint8_t mirrorLockupEnable, 
+                uint8_t mirrorLockupMinutes, uint8_t mirrorLockupSeconds, uint16_t mirrorLockupMilliseconds) {
+    mCamPortNumber = camPortNumber;
+    mMode = mode;
+    mDelayHours = delayHours;
+    mDelayMinutes = delayMinutes;
+    mDelaySeconds = delaySeconds;
+    mDelayMilliseconds = delayMilliseconds;
+    mDelayMicroseconds = delayMicroseconds;
+    mDurationHours = durationHours;
+    mDurationMinutes = durationMinutes;
+    mDurationSeconds = durationSeconds;
+    mDurationMilliseconds = durationMilliseconds;
+    mDurationMicroseconds = durationMicroseconds;
+    mSequencer = sequencer;
+    mApplyIntervalometer = applyIntervalometer;
+    mSmartPreview = smartPreview;
+    mMirrorLockupEnable = mirrorLockupEnable;
+    mMirrorLockupMinutes = mirrorLockupMinutes;
+    mMirrorLockupSeconds = mirrorLockupSeconds;
+    mMirrorLockupMilliseconds = mirrorLockupMilliseconds;
+    CA_ASSERT((mMode <= 2) && (mDelayHours <= 999) && (mDelayMinutes <=59) &&
+                (mDelaySeconds <= 59) && (mDelayMilliseconds <= 999) && (mDelayMicroseconds <= 999) &&
+                (mDurationHours <= 999) && (mDurationMinutes <= 59) && (mDurationSeconds <= 59) &&
+                (mDurationMilliseconds <= 999) && (mDurationMicroseconds <= 999) && (mApplyIntervalometer <= 1) &&
+                (mSmartPreview <= 59) && (mMirrorLockupEnable <= 1) && (mMirrorLockupMinutes <= 59) &&
+                (mMirrorLockupSeconds <= 59) && (mMirrorLockupMilliseconds <= 999),
+                "Error in CAPacketCamSettingsBase::set()");
+}
+
+///////////////////////////////////////////////////////////////////////////////
 // CamState Packet Class
 ///////////////////////////////////////////////////////////////////////////////
 CAPacketCamState::CAPacketCamState(CAPacket& caPacket) {
--- Sam3x/libraries/CAPacket/src/CAPacket.h
+++ Sam3x/libraries/CAPacket/src/CAPacket.h
@@ -1,7 +1,7 @@
 #ifndef __CAPACKET_H__
 #define __CAPACKET_H__
 
-#include "CAPacketBase.h"
+#include "CAUtility.h"
 
 enum packetId  {PID_START_SENTINEL      =  0,  // Must be first
                 PID_MENU_HEADER         =  1,
@@ -378,6 +378,64 @@ private:
     uint8_t mMultiplier;
     uint8_t mFocus;
     uint8_t mShutter;
+};
+
+class CAPacketCamSettingsBase {
+public:
+    const uint8_t SEQ0    = 0x01;
+    const uint8_t SEQ1    = 0x02;
+    const uint8_t SEQ2    = 0x04;
+    const uint8_t SEQ3    = 0x08;
+    const uint8_t SEQ4    = 0x10;
+    const uint8_t SEQ5    = 0x20;
+    const uint8_t SEQ6    = 0x40;
+    const uint8_t SEQ7    = 0x80;
+    CAPacketCamSettingsBase();
+    uint8_t getCamPortNumber() {return mCamPortNumber;};
+    uint8_t getMode() {return mMode;};
+    uint16_t getDelayHours() {return mDelayHours;};
+    uint8_t getDelayMinutes() {return mDelayMinutes;};
+    uint8_t getDelaySeconds() {return mDelaySeconds;};
+    uint16_t getDelayMilliseconds() {return mDelayMilliseconds;};
+    uint16_t getDelayMicroseconds() {return mDelayMicroseconds;};
+    uint16_t getDurationHours() {return mDurationHours;};
+    uint8_t getDurationMinutes() {return mDurationMinutes;};
+    uint8_t getDurationSeconds() {return mDurationSeconds;};
+    uint16_t getDurationMilliseconds() {return mDurationMilliseconds;};
+    uint16_t getDurationMicroseconds() {return mDurationMicroseconds;};
+    uint8_t getSequencer() {return mSequencer;};
+    uint8_t getApplyIntervalometer() {return mApplyIntervalometer;};
+    uint8_t getSmartPreview() {return mSmartPreview;};
+    uint8_t getMirrorLockupEnable() {return mMirrorLockupEnable;};
+    uint8_t getMirrorLockupMinutes() {return mMirrorLockupMinutes;};
+    uint8_t getMirrorLockupSeconds() {return mMirrorLockupSeconds;};
+    uint16_t getMirrorLockupMilliseconds() {return mMirrorLockupMilliseconds;};
+    void set(uint8_t camPortNumber, uint8_t mode, uint16_t delayHours, uint8_t delayMinutes,
+                uint8_t delaySeconds, uint16_t delayMilliseconds, uint16_t delayMicroseconds,
+                uint16_t durationHours, uint8_t durationMinutes, uint8_t durationSeconds, 
+                uint16_t durationMilliseconds, uint16_t durationMicroseconds, uint8_t sequencer,
+                uint8_t applyIntervalometer, uint8_t smartPreview, uint8_t mirrorLockupEnable, 
+                uint8_t mirrorLockupMinutes, uint8_t mirrorLockupSeconds, uint16_t mirrorLockupMilliseconds);
+protected:
+    uint8_t mCamPortNumber;
+    uint8_t mMode;
+    uint16_t mDelayHours;
+    uint8_t mDelayMinutes;
+    uint8_t mDelaySeconds;
+    uint16_t mDelayMilliseconds;
+    uint16_t mDelayMicroseconds;
+    uint16_t mDurationHours;
+    uint8_t mDurationMinutes;
+    uint8_t mDurationSeconds;
+    uint16_t mDurationMilliseconds;
+    uint16_t mDurationMicroseconds;
+    uint8_t mSequencer;
+    uint8_t mApplyIntervalometer;
+    uint8_t mSmartPreview;
+    uint8_t mMirrorLockupEnable;
+    uint8_t mMirrorLockupMinutes;
+    uint8_t mMirrorLockupSeconds;
+    uint16_t mMirrorLockupMilliseconds;
 };
 
 class CAPacketCamSettings : public CAPacketCamSettingsBase, public CAPacketElement  {
