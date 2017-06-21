5ca2da6150a21f962c8cddfd7b946a89e963e1ef
Maurice Ribble <maurice@dreamingrobots.com>
2017-06-21 14:56:55 -0400

    Remove a bunch of files no longer needed for the web_server approach

--- CommonLibraries/CAStd/CAStd_esp8266.h
+++ CommonLibraries/CAStd/CAStd_esp8266.h
@@ -5,7 +5,7 @@
 #define CA_DEBUG_ASSERT
 #define CA_DEBUG_INFO
 #define CA_DEBUG_ERROR
-#define SerialIO Serial1
+#define SerialIO Serial
 //#define SerialIO Serial1               // UART on GPIO2 - use a pullup (this is necessary for boot anyway)
 
 #include <CAStd.h>
