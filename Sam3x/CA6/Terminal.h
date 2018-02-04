2b64037e547d4f917a8af6a4904bee97d5f2a3ca
Maurice Ribble <maurice@dreamingrobots.com>
2018-02-04 14:17:10 -0500

    Start work on code to transfer data to led flash.  Update pcb to improve clearance.

--- Sam3x/CA6/Terminal.h
+++ Sam3x/CA6/Terminal.h
@@ -7,6 +7,30 @@
 #ifndef TERMINAL_H
 #define TERMINAL_H
 
+#define CA_QUANTUM 10  // This is the base time quantium in microseconds
+
+// This uses a simple protocal to transfer 1 byte (8 bits).  The protocal can only send data one way becuase it is
+// designed to work over camera trigger ports which are basically just 2 switched on the transmitter.  Then the
+// receiver has two lines connected to those switches with pullup resistors so when when the transmitter is
+// low the receiver is high and when the transmitter is high the reciever is low.
+// 
+// This protocal transfer data over a 3.5mm jack.  There are 3 connections (tip transfers data, ring enables 
+// data transfers, and base is common/ground).  Here is how data is transfered:
+// 1) Ring is set high
+// 2) Tip set low for 4 quantums (init period)
+// 3) For each bit:
+// 4)   if bit is 0: tip set high for 1 quantums, then tip set low for 2 quantums
+// 5)   if bit is 1: tip set high for 2 quantums, then tip set low for 1 quantums
+// 6) Tip set high for 4 quantums (end period)
+// 7) Ring is set low
+//
+void txCaByte(uint8_t, val) {
+  hwPortPin ppTip = CAU::getModulePin(0, FOCUS);   // 3.5mm ring
+  hwPortPin ppRing = CAU::getModulePin(0, SHUTTER); // 3.5mm tip
+  
+  CAU::pinMode(pp, OUTPUT);  // Set pins to high impedance
+}
+
 void processTerminalCmds() {
   if (SerialIO.available()) {
     uint8_t command = SerialIO.read();
